7a1be5248c5ab22af1c3a2f1c26d5fa2579d89f6
avener <eavener@gmail.com>
2021-11-04 20:53:04 -0400

    Studio for class 12

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,33 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "username blank")
+    @Size(min = 5, max = 15, message = "username size wrong")
     private String username;
+
+    @Email(message = "email")
     private String email;
+
+    @NotBlank(message = "pword blank")
+    @NotNull
+    @Size(min = 6, message = "pword must be 6")
     private String password;
+
+    @NotBlank
+    @NotNull(message = "Passwords do not match")
+    private String verify;
+
+    private void checkPassword() {
+        if(this.password != this.verify) {
+            verify = null;
+        }
+    }
 
     public User() {
 
@@ -14,6 +38,7 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -38,5 +63,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
+        checkPassword();
     }
 }
